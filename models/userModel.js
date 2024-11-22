cc6de7090afca619b79fa835e67d97db29fa135a
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-11-22 17:24:39 -0300

    Add user model

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from "npm:mongoose";
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      minLength: 3,
+      maxLength: 32,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      minLength: 5,
+      maxLength: 256,
+      unique: true,
+      required: true,
+    },
+    password: { type: String, minLength: 3, maxLength: 512, required: true },
+    profilePicture: { type: String },
+    bio: { type: String, default: "This is a boring bio...", maxLength: 512 },
+    role: {
+      type: String,
+      enum: ["basic", "admin"],
+      default: "basic",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
